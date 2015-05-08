aba1b7d712918c17c55422e8a1419b37ca28b897
Tom-Aerts <tom.aerts@commercieelwebverkeer.be>
2015-05-08 10:43:49 +0200

    back-up before cleaning up the code
    back-up before cleaning up the code

--- website lagere school/scripts/konami.js
+++ website lagere school/scripts/konami.js
@@ -13,7 +13,7 @@
                 input.shift();
 
                 if (input.length == 0) {
-                    return alert("You are awesome");
+                    window.location.href = "http://7cw-01.commercieelwebverkeer.be/LagereSchool/konami.html";
                 }
             } else {
                 input = command.slice(0);
